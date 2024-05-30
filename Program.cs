aa86a53fe828fa1b2471ed070c25e99f5cfc44d3
Bloszom <blossom.giane@decagon.dev>
2024-05-30 22:30:46 +0100

    Coure Tech Technical Assessment Question1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Buffers.Text;
+using CoureTechQuestion1;
+
+namespace CoureTechQuestion1
+{
+    public class Program
+    { 
+        static void Main(string[] args)
+        {           
+
+            int[] array1 = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 15, 25, 35 };
+            int[] array3 = { 8, 8 };
+
+            int totalScore1 = TotalScore.CalculateScore(array1); 
+            Console.WriteLine("Total Score1: " + totalScore1); 
+
+            int totalScore2 = TotalScore.CalculateScore(array2); 
+            Console.WriteLine("Total Score2: " + totalScore2); 
+
+            int totalScore3 = TotalScore.CalculateScore(array3);
+            Console.WriteLine("Total Score3: " + totalScore3); 
+
+        }
+      
+    }
+}
---
+++ TotalScore.cs
@@ -0,0 +1,28 @@
+﻿namespace CoureTechQuestion1
+{
+    public class TotalScore
+    {
+        public static int CalculateScore(int[] numbers)
+        {
+            int score = 0;
+            foreach (int num in numbers)
+            {
+                if (num == 8)
+                {
+                    score += 5;
+                }
+                if (num % 2 == 0)
+                {
+                    score += 1;
+                }
+                else
+                {
+                    score += 3;
+                }
+            }
+            return score;
+
+        }
+    }
+      
+}
